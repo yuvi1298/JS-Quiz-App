59e3eb14f9ba9986d330fbbf1848160f9d1cfd5d
yuvi1298 <raja29102001singh@gmail.com>
2024-01-17 22:07:51 +0530

    First Commit

---
+++ quiz.js
@@ -0,0 +1,87 @@
+const questions = [
+  {
+    'que': "Which of the following is a markup language?",
+    'a': "HTML",
+    'b': "CSS",
+    'c': "JavaScript",
+    'd': "Php",
+    'correct': "a"
+  },
+  {
+    'que': "Which of the following is a markup language?",
+    'a': "HTML",
+    'b': "CSS",
+    'c': "JavaScript",
+    'd': "Php",
+    'correct': "b"
+  },
+  {
+    'que': "Which of the following is a markup language?",
+    'a': "HTML",
+    'b': "CSS",
+    'c': "JavaScript",
+    'd': "Php",
+    'correct': "c",
+  }
+]
+let index = 0;
+let total=questions.length;
+let right=0,
+ wrong=0;
+const quesBox = document.getElementById("quesBox")
+const optionInputs = document.querySelectorAll(".options")
+const loadQuestion = () => {
+  if(index===total){
+    return endQuiz()
+
+  }
+  reset()
+  const data = questions[index]
+  quesBox.innerText = `${index+1}) ${data.que}`;
+  optionInputs[0].nextElementSibling.innerText = data.a;
+  optionInputs[1].nextElementSibling.innerText = data.b;
+   optionInputs[2].nextElementSibling.innerText = data.c;
+  optionInputs[3].nextElementSibling.innerText = data.d;
+}
+
+const submitQuiz=()=>{
+  const data=questions[index];
+  const ans=getAnswer()
+  if(ans==data.correct){
+    right++;
+  }else{
+    wrong++;
+  }
+  index++;
+  loadQuestion();
+  return;
+}
+const getAnswer=()=>{
+  let answer;
+  optionInputs.forEach(
+    (input)=>{
+      if(input.checked){
+        answer= input.value;
+
+      }
+    }
+  )
+  return answer;
+}
+const reset=()=>{
+  optionInputs.forEach(
+    (input)=>{
+      input.checked=false
+
+    }
+  )
+
+}
+const endQuiz=()=>{
+  document.getElementById("box").innerHTML=`<div style="text-align:center">
+  <h3>Thanks For playng The Quiz</h3>
+  <h2>${right}/${total} are correct</h2>
+  </div>`
+}
+//initial call
+loadQuestion();
